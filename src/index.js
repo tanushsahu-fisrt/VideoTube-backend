15a12397cb2da6b1fad32ad10ba5e56e14834aef
tanushsahu-fisrt <sahutanush29@gmail.com>
2024-09-16 00:00:51 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+
+})
+.catch( (error) => {
+    console.log("MOngodb connection failed",error)
+})
 
 
 
