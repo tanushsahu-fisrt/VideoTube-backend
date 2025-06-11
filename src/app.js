6d0f5133f7f87093d17d020065bd0f29786ce8d7
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-06-11 17:18:04 +0530

    changes in server file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -17,9 +17,9 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
 //routes import
 import userRouter from "./routes/user.routes.js"
-
 app.use("/api/v1/users",userRouter)
 
 export { app };
--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 // import { Tanush_DB } from ".db/constant.js"
 
-const connectDB = async ()=>{
+const connectDB = async () => {
     try{
         const ConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.Tanush_DB}`);
         console.log(`mongoDB connected: ${ConnectionInstance.connection.host}`)
@@ -15,4 +15,3 @@ const connectDB = async ()=>{
 
 
 export default connectDB
-
