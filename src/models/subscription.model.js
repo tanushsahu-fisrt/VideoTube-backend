e852e9f01b21d6ba5f8ce810c63ae3d45546ad4a
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-06-15 22:53:14 +0530

    added models and controllers fot todos

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,16 +1,16 @@
-import mongoose,{ Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose"
 
 const subscriptionSchema = new Schema({
-    Subscriber : {
-        type : Schema.Types.ObjectId, // one who is subscribing
-        ref : "User"
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User"
     },
-    channel : {
-        type : Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
-        ref : "User"
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+        ref: "User"
     }
-},{
-    timestamps : true,
-})
+}, {timestamps: true})
 
-export const Subscription = mongoose.model('Subscription',subscriptionSchema);
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
