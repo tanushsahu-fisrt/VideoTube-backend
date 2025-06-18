c4c9b0fd538cd8d67ac5284a5aaa5969c551a74f
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-06-18 10:15:33 +0530

    added all tweet controllers

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,20 +5,115 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body; 
+
+    if(!content){
+        throw new ApiError(404,"Tweet content is empty");
+    }
+
+    const user = req.user?._id;
+
+    if(!user){
+        throw new ApiError(404,"Unable to get User");
+    }
+
+    const postTweet = await Tweet.create({
+        content,
+        owner : user
+    })
+
+    if(!postTweet){
+        throw new ApiError(404,"Unable to Create Tweet");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,postTweet,"Tweet created")
+    )
+
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const { userId } = req.params;
+    
+    if(!userId){
+        throw new ApiError(404,"Unable to get User");
+    }
+
+    const userTweet = await Tweet.find({ owner : userId });
+
+    if(userTweet.length === 0){
+       return res
+        .status(200)
+        .json(
+        new ApiResponse(200,userTweet,"No tweets by the user")
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,userTweet,"user Tweet fetched")
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {content} = req.body;
+    const {tweetId} = req.params;
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(404,"Tweet Id is undefined");
+    }
+
+    if(!content){
+        throw new ApiError(404,"Unable to get Tweet content");
+    }
+
+    const updateTweet = await Tweet.findByIdAndUpdate(
+            tweetId,
+            {
+                $set : {
+                    content : content
+                }
+            },
+            { new : true}
+    )
+
+    if(!updateTweet){
+        throw new ApiError("Unable to Update Tweet");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,updateTweet,"Tweet Updated")
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params;
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(404,"Tweet Id is invalid");
+    }
+
+    const deleteTweet = await Tweet.findByIdAndDelete(tweetId);
+
+    if(!deleteTweet){
+        throw new ApiError("Unable to Delete Tweet");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{},"Tweet Deleted")
+    )
 })
 
 export {
