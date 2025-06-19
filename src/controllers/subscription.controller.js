fce50b0f7193b7ae76a69580d45b450a18652040
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-06-19 11:08:34 +0530

    added subscriber controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,19 +6,82 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
+// TODO: toggle subscription
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
+    
+    const {channelId} = req.params;
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(404,"Channel id is not valid");
+    }
+
+    const userId = req.user?._id;
+
+    if(!userId){
+        throw new ApiError(404,"User not found")
+    }
+
+    const existingSubscriber = await Subscription.findOne({ 
+        subscriber : userId,
+        channel : channelId
+    })
+
+    if(existingSubscriber){
+        const deleteExistingSubscriber =  await Subscription.deleteOne({ _id : existingSubscriber?._id })
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,{},"existing subscriber deleted")
+        )
+    }
+
+    const createSubscriber = await Subscription.create({
+        subscriber : userId,
+        channel : channelId
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, createSubscriber ,"Subscriber Created")
+    )
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const {subscriberId} = req.params;
+
+    if(!isValidObjectId(subscriberId)){
+        throw new ApiError(404,"Channel id is not valid");
+    }
+
+    const channelSubscribers = await Subscription.find({ subscriber : subscriberId })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channelSubscribers ,"fetched subscriber list of channel")
+    )
+
 })
 
-// controller to return channel list to which user has subscribed
+// controller to return channel list to which user has subscribed 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+    const { channelId } = req.params;
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(404,"Channel id is not valid");
+    }
+
+    const userChannelSubscribers = await Subscription.find({ channel : channelId })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, userChannelSubscribers ,"Fetched list of channels subscribed by the user")
+    )
 })
 
 export {
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -9,11 +9,11 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router
-    .route("/c/:channelId")
+router.route("/c/:channelId")
     .get(getSubscribedChannels)
     .post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId")
+    .get(getUserChannelSubscribers);
 
 export default router
